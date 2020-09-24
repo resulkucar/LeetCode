07e48361db4cf371b2be87548cc33a22dcc7f272
Resul Ucar <rucar@purdue.edu>
2020-09-23 23:29:03 -0400

    updated formatting

--- runningsum.java
+++ runningsum.java
@@ -1,6 +1,9 @@
-class Solution { public int[] runningSum(int[] nums) { int a [] = new int [nums.length]; for(int 
-        i=0;i<nums.length;i++) {
-            int sum = 0; for(int k=0; k<=i;k++) { sum += nums[k];
+class Solution { 
+        public int[] runningSum(int[] nums) { 
+        int a [] = new int [nums.length]; 
+        for(int i=0;i<nums.length;i++) {
+            int sum = 0; for(int k=0; k<=i;k++){ 
+                    sum += nums[k];
             }
             a[i] = sum;
         }

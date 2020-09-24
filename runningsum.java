c892ddd369ff3f01fa3b486716da6f4250d4f02e
Resul Ucar <rucar@purdue.edu>
2020-09-23 23:26:18 -0400

    running sum solved

---
+++ runningsum.java
@@ -0,0 +1,9 @@
+class Solution { public int[] runningSum(int[] nums) { int a [] = new int [nums.length]; for(int 
+        i=0;i<nums.length;i++) {
+            int sum = 0; for(int k=0; k<=i;k++) { sum += nums[k];
+            }
+            a[i] = sum;
+        }
+        return a;
+    }
+}
